f5e67484de5957b31a799679863815ab2808079f
Rui Li <li.rui@ge.com>
2013-06-01 02:19:29 -0400

    add config.cmake

---
+++ GeneratePythonCppAPIConfig.cmake
@@ -0,0 +1,24 @@
+# Generate the PythonCppAPIConfig.cmake file in the buid tree
+
+# Settings specific to the build tree
+#
+set(PythonCppAPI_INCLUDE_DIR_CONFIG
+  ${ModuleDescriptionParser_INCLUDE_DIR}
+  )
+
+set(PythonCppAPI_LIBRARY_DIR_CONFIG
+  ${CMAKE_ARCHIVE_OUTPUT_DIRECTORY}
+  ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}
+  )
+
+set(PythonCppAPI_USE_FILE_CONFIG
+  ${PythonCppAPI_BINARY_DIR}/UsePythonCppAPI.cmake
+  )
+
+# configure PythonCppApiConfig.cmake for the build tree
+#
+configure_file(
+  ${PythonCppAPI_SOURCE_DIR}/PythonCppApiConfig.cmake.in
+  ${PythonCppAPI_BINARY_DIR}/PythonCppApiConfig.cmake
+  @ONLY
+  )
